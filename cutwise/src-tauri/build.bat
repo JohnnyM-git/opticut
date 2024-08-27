cbd13acb766b843ce2f6d00e5c6fd1ea9749195b
Johnny <johnny.martens@protonmail.com>
2024-08-26 20:48:03 -0400

    OPTICUT

--- cutwise/src-tauri/build.bat
+++ cutwise/src-tauri/build.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Building for Windows...
 cd ..\main
-go build -o ..\cutwise\src-tauri\resources\main\main.exe -ldflags=-H=windowsgui
+go build -o ../cutwise/src-tauri/resources/main/main.exe -ldflags=-H=windowsgui
