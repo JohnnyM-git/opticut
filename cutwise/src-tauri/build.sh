362d39cc8dc49a23f1d07894d8075936839ac33c
User <johnny@johnnym.me>
2024-09-02 15:59:18 -0400

    OPTICUT

--- cutwise/src-tauri/build.sh
+++ cutwise/src-tauri/build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Build the Linux executable
 cd ../main && go build -o ../cutwise/src-tauri/resources/main/main_unix
+
+# Build the Windows executable (optional in a Linux CI environment)
+go build -o ../cutwise/src-tauri/resources/main/main -ldflags="-H=windowsgui"
+
+# Copy the settings.json file
+cp ../main/globals/settings.json ../cutwise/src-tauri/resources/globals/settings.json
+
+# Copy the setup_db.sql file
+cp ../main/internal/db/setup_db.sql ../cutwise/src-tauri/resources/main/internal/db/setup_db.sql
