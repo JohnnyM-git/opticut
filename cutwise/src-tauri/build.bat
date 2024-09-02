49dc2afb356a7a7846fcf936da30daa312b54882
User <johnny@johnnym.me>
2024-09-02 14:58:52 -0400

    OPTICUT

--- cutwise/src-tauri/build.bat
+++ cutwise/src-tauri/build.bat
@@ -1,4 +1,17 @@
 @echo off
 echo Building for Windows...
+
+:: Change to the directory containing your Go code
 cd ..\main
+
+:: Build the Go executable
 go build -o ../cutwise/src-tauri/resources/main/main.exe -ldflags=-H=windowsgui
+
+:: Copy settings.json to the resources directory
+copy ..\main\globals\settings.json ..\cutwise\src-tauri\resources\globals\settings.json
+
+:: Copy setup_db.sql to the resources directory
+copy ..\main\internal\db\setup_db.sql ..\cutwise\src-tauri\resources\main\internal\db\setup_db.sql
+
+
+echo Build and file copying completed.
