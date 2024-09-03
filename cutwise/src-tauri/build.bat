ea02b5a1e95e611b99df6703d252c57e25ed8f3e
Johnny Martens <johnny.martens@protonmail.com>
2024-09-03 08:05:16 -0400

    OPTICUT

--- cutwise/src-tauri/build.bat
+++ cutwise/src-tauri/build.bat
@@ -13,5 +13,9 @@ copy ..\main\globals\settings.json ..\cutwise\src-tauri\resources\globals\settin
 :: Copy setup_db.sql to the resources directory
 copy ..\main\internal\db\setup_db.sql ..\cutwise\src-tauri\resources\main\internal\db\setup_db.sql
 
+cd ..\cutwise
+
+pnpm tauri build
+
 
 echo Build and file copying completed.
